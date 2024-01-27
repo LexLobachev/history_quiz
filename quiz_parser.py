bb94736dc7adb9de7e5932f985c40db0a02ea9e9
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2024-01-27 20:03:09 +0400

    delete prints

--- quiz_parser.py
+++ quiz_parser.py
@@ -7,17 +7,12 @@ def open_file():
         for line in lines:
             question_number = f'Вопрос {question_id}:'
             if 'Вопрос' in line:
-                print(question_number)
                 question = line.lstrip().replace('\n', ' ').split(': ', 1)[1]
-                print(question)
                 quiz.setdefault(question_number, []).append(question)
             elif 'Ответ' in line:
                 answer = line.lstrip().replace('\n', ' ')
                 quiz.setdefault(question_number, []).append(answer)
-                print(answer)
                 question_id += 1
-
-    print(quiz)
 
 
 if __name__ == '__main__':
