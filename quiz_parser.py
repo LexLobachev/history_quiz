dc92375eb931e3b89cd2efd60b053e26392558a4
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2024-01-31 02:05:35 +0400

    add path for parse

--- quiz_parser.py
+++ quiz_parser.py
@@ -1,7 +1,9 @@
+from decouple import config
+
 
 def load_quiz_questions():
     quiz = {}
-    with open("quiz-questions/1vs1200.txt", "r", encoding="KOI8-R") as my_file:
+    with open(config("PATH_FOR_PARSE"), "r", encoding="KOI8-R") as my_file:
         lines = my_file.read().split('\n\n')
         question_id = 1
         for line in lines:
